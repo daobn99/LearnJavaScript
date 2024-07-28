d4da52e26fdf35d913a2e5ec4ea23fcf521ecab1
Dao Nguyen <lmb29x178z@gmail.com>
2024-07-29 00:48:31 +0900

    add file js

---
+++ JavaScriptBasic/a.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class a {
+    public static void main(String[] args) {
+        String[] arr = {"dao", "nguyen"};        
+        System.out.println(Arrays.toString(arr));
+
+        int[] numbers = {45, 4, 9, 16, 25};
+        for (int number : numbers){
+            System.out.println(number);
+        }
+    }
+}
